d79dbdf398e96a6502d34ab890dc400657554b73
rockyhendersonPC <jack@teamhendo.com>
2024-09-05 18:17:43 +0100

    UUID local storage update

--- main.js
+++ main.js
@@ -4,7 +4,20 @@ function getuuid() {
     .then(data => {
       const UUID = data.userId; // Store the userId in a variable called UUID
       console.log("UUID:", UUID); // Log the UUID to the console
-      document.getElementById("message").innerText = `User ID: ${UUID}`; // Update the DOM with the UUID
+
+      // Save the UUID to localStorage
+      localStorage.setItem("uuid", UUID);
+
+      // Update the DOM with the UUID
+      document.getElementById("message").innerText = `${UUID}`;
     })
     .catch(error => console.error('Error fetching data:', error));
+}
+let UUID = localStorage.getItem("uuid");
+
+if (UUID){
+  console.log("UUID from local storage is ", UUID);
+  document.getElementById("message").innerText = '${UUID}';
+}else{
+  console.log("no UUID locally stored")
 }
