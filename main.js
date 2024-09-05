a85788da80fe4aff81a2b19e5e3ca94f44b612d2
rockyhendersonPC <jack@teamhendo.com>
2024-09-05 22:14:17 +0100

    removed testing functions

--- main.js
+++ main.js
@@ -31,7 +31,10 @@ function fetchUUIDAndData(username) {
 
 // Function to display the data in the DOM
 function displayData(UUID, username, skyblockData) {
-  document.getElementById("message").innerText = `UUID: ${UUID}`;
+  // document.getElementById("message").innerText = `UUID: ${UUID}`;
+  // document.getElementById("usernameDisplay").innerText = `Username: ${username}`;
+  // document.getElementById("skyblockDataDisplay").innerText = `Skyblock Data: ${JSON.stringify(skyblockData, null, 2)}`;
+  //TESTING DOCUMENTS
 }
 
 // Function to load data from cache or fetch new data
