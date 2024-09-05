f086332dbec8ef242290e8b59b9684ec20edd98e
rockyhendersonPC <jack@teamhendo.com>
2024-09-05 16:27:27 +0100

    test

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 document.getElementById("fetchMessage").addEventListener("click", () => {
-    fetch("/netlify/functions/hello")
-      .then(response => response.json())
-      .then(data => {
-        document.getElementById("message").innerText = data.message;
-      })
-      .catch(error => console.error('Error fetching data:', error));
-  });
-  
+  fetch("/netlify/functions/hello")
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("message").innerText = data.message;
+    })
+    .catch(error => console.error('Error fetching data:', error));
+});
