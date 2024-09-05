4331e06f8dc4994095fe3e3de3fdf44a94c2152c
rockyhendersonPC <jack@teamhendo.com>
2024-09-05 23:02:27 +0100

    adding pulling from local storage farming

--- farming.js
+++ farming.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Read the data from localStorage
+  const cachedSkyblockData = localStorage.getItem("skyblockData");
+  if (cachedSkyblockData) {
+    const skyblockData = JSON.parse(cachedSkyblockData);
+    console.log(skyblockData.profile); // Output: defaultProfile
+  } else {
+    console.log("No skyblock data found in local storage.");
+  }
+  
+  let x = 0.5;
   var bar = new ProgressBar.Line("#progress-bar", {
     strokeWidth: 4,
     easing: "easeInOut",
@@ -7,7 +17,10 @@ document.addEventListener("DOMContentLoaded", function () {
     trailColor: "#eee",
     trailWidth: 1,
     svgStyle: { width: "100%", height: "100%" },
+    step: function (state, bar) {
+      bar.path.setAttribute("stroke-linecap", "round"); // Round the edges of the bar
+    },
   });
 
-  bar.animate(1.0); // Number from 0.0 to 1.0
+  bar.animate(x); // Number from 0.0 to 1.0
 });
