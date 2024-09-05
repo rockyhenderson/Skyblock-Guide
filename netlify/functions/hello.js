206b4c76ebb9f783c98711f8565f714196e60c6d
rockyhendersonPC <jack@teamhendo.com>
2024-09-05 16:47:58 +0100

    test

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,20 +1,23 @@
 exports.handler = async function(event, context) {
   try {
-    const response = await fetch('https://api.mojang.com/users/profiles/minecraft/hatnehyt?'); // Make a request to the test API
+    const response = await fetch('https://api.mojang.com/users/profiles/minecraft/hatnehyt'); // Make a request to the Mojang API
     const apiData = await response.json(); // Parse the response as JSON
+
+    // Extract only the "id" from the API response
+    const { id } = apiData;
 
     return {
       statusCode: 200,
       body: JSON.stringify({ 
-        apiResponse: apiData // Return the API response to the front-end
+        userId: id  // Return only the user ID
       })
     };
   } catch (error) {
-    console.error('Error making request to testapi:', error);
+    console.error('Error making request to Mojang API:', error);
     return {
       statusCode: 500,
       body: JSON.stringify({ 
-        message: 'Error fetching data from testapi'
+        message: 'Error fetching data from Mojang API'
       })
     };
   }
