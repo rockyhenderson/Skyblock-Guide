1c55b51cbf612e5bb7e1294e736540ea995787de
Rockyhenderson <jack@teamhendo.com>
2024-09-10 15:21:35 +0100

    re parsed farming back in

--- dataManager.js
+++ dataManager.js
@@ -11,6 +11,7 @@ function fetchData(username) {
         // Step 2: Log the data to the console for testing
         console.log("Player Level:", data.playerLevel);
         console.log("Player UUID:", data.playerUUID);
+        console.log("Player Skills", data.playerFarmingSkill)
       })
       .catch((error) => console.error("Error fetching data:", error));
   }
--- netlify/functions/DataRequest.js
+++ netlify/functions/DataRequest.js
@@ -36,6 +36,7 @@ exports.handler = async function (event, context) {
       body: JSON.stringify({
         playerLevel: player.level, // Hypixel level
         playerUUID: player.uuid,
+        playerFarmingSkill: player.SkyblockSkillLevel.farming,
       }),
     };
   } catch (error) {
